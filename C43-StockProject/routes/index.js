d615c66d97bc039c0842e0435a5db0fa8068d7f1
Vraj2004 <vrajr.shah@mail.utoronto.ca>
2025-03-31 20:21:52 -0400

    Set up the database connection

--- C43-StockProject/routes/index.js
+++ C43-StockProject/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db/db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  try {
+    const result = await db.query('SELECT * FROM users');
+    res.render('index', { title: 'User List', users: result.rows });
+  } catch (err) {
+    console.error('Error fetching users:', err);
+    res.status(500).send('Error fetching data');
+  }
 });
 
 module.exports = router;
